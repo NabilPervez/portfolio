1da37ebbca855b1c074ade6cacab19ceb68c41ee
Nabil Pervez <nabilpervez@Nabils-MacBook-Pro.local>
2026-01-26 08:35:35 -0600

    Allow FadeIn component to accept id and other props to fix build error

--- web/components/FadeIn.tsx
+++ web/components/FadeIn.tsx
@@ -2,13 +2,14 @@
 
 import { motion } from "framer-motion";
 
-export const FadeIn = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+export const FadeIn = ({ children, className, ...props }: { children: React.ReactNode, className?: string, [key: string]: any }) => {
     return (
         <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
             className={className}
+            {...props}
         >
             {children}
         </motion.div>
